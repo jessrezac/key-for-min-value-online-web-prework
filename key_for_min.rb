916b2108a55b059f9ed41b49183458136f566723
Jess Rezac <jess.rezac@gmail.com>
2019-04-27 19:58:54 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -19,5 +19,5 @@ def key_for_min_value(name_hash)
   return min_key
 end
 
-ikea = {:chair => 25, :table => 85, :mattress => 450}
-key_for_min_value(ikea)
+veggies = {"apple" => -45, "banana" => -44.5, "carrot" => -44.9}
+key_for_min_value(veggies)

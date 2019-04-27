5a2180d79515449833efb032631b94c610f1a40e
Jess Rezac <jess.rezac@gmail.com>
2019-04-27 19:58:01 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,14 +6,16 @@ def key_for_min_value(name_hash)
   name_hash.each do |item, cost|
     if min_value == 0
       min_value = cost
+      min_key = item
     elsif cost < min_value
       min_value = cost
+      min_key = item
     end
   end
 
-  puts min_value
+  puts min_key
 
-  return min_value
+  return min_key
 end
 
 ikea = {:chair => 25, :table => 85, :mattress => 450}

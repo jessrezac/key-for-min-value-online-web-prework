bd103130a2ac561d316c385a56dbab84633714d1
Jess Rezac <jess.rezac@gmail.com>
2019-04-27 19:57:21 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   min_value = 0
   name_hash.each do |item, cost|
-    if min_value = 0
+    if min_value == 0
       min_value = cost
     elsif cost < min_value
       min_value = cost
